58981d491bc8ee3561c4b6979c27c9c2acac5a4f
IamRaul <jarquinr121@gmail.com>
2023-05-18 11:24:17 -0700

    added nav bar/first commit

---
+++ my_game/somegame/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import '@picocss/pico'
+</script>
+
+<article>
+<nav class="container-fluid">
+  <ul>
+    <li><strong>Notess</strong></li>
+  </ul>
+  <ul>
+    <li><a href="">Change</a></li>
+    <li><a href="">Past notes</a></li>
+  </ul>
+</nav>
+</article>
+
+<style>
+  article{
+    padding:0px;
+    margin:0px;
+}
+  nav li {
+  display: inline-block;
+  margin-right: 10px; /* Example margin between items */
+}
+</style>
